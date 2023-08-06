20bad343a539fb7dce4a2f53643bc9d950936fe1
Jason Wiles <wiles.jason@gmail.com>
2023-08-06 09:41:54 -0600

    Updates

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,24 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-  return gulp.src('app/scss/styles.scss')
+  return gulp.src('app/scss/style.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('app/css/style.css'));
-};
+    .pipe(gulp.dest('dist/css/'));
+}
+function copyIndex() {
+    return gulp.src('app/index.html')
+        .pipe(gulp.dest('dist'))
+}
+function copyImg() {
+    return gulp.src('app/img/*')
+        .pipe(gulp.dest('dist/img'))
+}
+function copyJs() {
+    return gulp.src('app/js/*')
+        .pipe(gulp.dest('dist/js'))
+}
 
 exports.buildStyles = buildStyles;
-exports.watch = function () {
-  gulp.watch('app/scss/styles.scss', ['sass']);
-};
+exports.copyIndex = copyIndex;
+exports.copyImg = copyImg;
+exports.copyJs = copyJs;
