484faea27d923d9763d5a97f856be009decea50b
Jason Wiles <wiles.jason@gmail.com>
2023-08-07 11:05:17 -0600

    Updates

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-  return gulp.src('app/scss/style.scss')
+  return gulp.src('app/scss/style3.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('dist/css/'));
 }
